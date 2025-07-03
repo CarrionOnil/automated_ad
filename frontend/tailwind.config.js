92f505193146ff57df625cc856d0ea9511440fbd
Onil Carrion <ocarrion@pqcontrols.local>
2025-07-03 10:00:30 -0400

    Frontend initalized

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,20 @@
+// module.exports = {
+//   purge: [],
+//   darkMode: false, // or 'media' or 'class'
+//   theme: {
+//     extend: {},
+//   },
+//   variants: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],  // 👈 this tells Tailwind where to look
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
